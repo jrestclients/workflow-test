45e50c84a8b5abdd7557a96975d1aeea68df1a9b
Martin d'Anjou <martin.danjou14@gmail.com>
2021-10-17 20:27:38 -0400

    Feature hello 6

--- src/main/java/ca/martinda/App.java
+++ src/main/java/ca/martinda/App.java
@@ -15,5 +15,6 @@ public class App {
         System.out.println("Hello 3");
         System.out.println("Hello 4");
         System.out.println("Hello 5");
+        System.out.println("Hello six");
     }
 }
