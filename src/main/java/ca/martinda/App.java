d98dca976bd8bba6b41d801c3492262302342c99
Martin d'Anjou <martin.danjou14@gmail.com>
2021-10-30 15:12:54 -0400

    Feature 11

--- src/main/java/ca/martinda/App.java
+++ src/main/java/ca/martinda/App.java
@@ -19,5 +19,6 @@ public class App {
         System.out.println("Hello sept");
         System.out.println("Hello huit");
         System.out.println("Hello 10 (pas de 9)");
+        System.out.println("Hello 11");
     }
 }
